1a84653a935f522de1e5b5462dccb45fd8217f50
Tiery <tierymail@gmail.com>
2012-07-20 18:32:17 +0200

    add template 000_patterns.php

---
+++ templates/000_patterns.php
@@ -0,0 +1,9 @@
+<?php
+$templateName = 'Patterns';
+?>
+<?php include_once('../config.php'); ?>
+<?php include_once(_ROOT_ . 'includes/head.php'); ?>
+
+
+
+<?php include_once(_ROOT_ . 'includes/foot.php'); ?>
