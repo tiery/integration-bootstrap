e8687cd6910191441f3ae35a5077fb243f2c4a59
tiery <tierymail@gmail.com>
2012-07-25 22:54:02 +0200

    ajustement patterns

--- templates/000_patterns.php
+++ templates/000_patterns.php
@@ -5,40 +5,16 @@ $name = 'Patterns';
 <?php include_once(_ROOT_ . 'includes/head.php'); ?>
 
 <ul id="patterns-menu">
-	<li><a href="#s1">Layout</a></li>
-	<li><a href="#s2">Grid</a></li>
-	<li><a href="#s3">Buttons</a></li>
-	<li><a href="#s4">Lists</a></li>
-	<li><a href="#s5">Forms</a></li>
+	<li><a href="#s1">Section 1</a></li>
 </ul>
 
 <div id="patterns">
 	
 	
 	<div id="s1" class="section">
-		<h1>Layout</h1>
+		<h1>Section 1</h1>
 		<h2>Sub title</h2>
 	</div><!-- #s1 -->
-	
-	<div id="s2" class="section">
-		<h1>Grid</h1>
-		<h2>Sub title</h2>
-	</div><!-- #s2 -->
-	
-	<div id="s3" class="section">
-		<h1>Buttons</h1>
-		<h2>Sub title</h2>
-	</div><!-- #s3 -->
-	
-	<div id="s4" class="section">
-		<h1>Lists</h1>
-		<h2>Sub title</h2>
-	</div><!-- #s4 -->
-	
-	<div id="s5" class="section">
-		<h1>Forms</h1>
-		<h2>Sub title</h2>
-	</div><!-- #s5 -->
 	
 </div>
 
