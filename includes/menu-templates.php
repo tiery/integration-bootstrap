581fa3c85620764cf15163fc306a5f1d89105f08
tiery <tierymail@gmail.com>
2012-07-26 00:00:24 +0200

    modif menu templates

--- includes/menu-templates.php
+++ includes/menu-templates.php
@@ -11,15 +11,34 @@ if ($dir = @opendir($mydir)) {
 if(sizeof($filelist) != '0') {
 ?>
 <dl id="templates">
-	<dt>Templates</dt>
+	<dt>TPL
+	<?php
+	function lastIndexOf($string,$item){  
+	    $index=strpos(strrev($string),strrev($item));  
+	    if ($index){  
+	        $index=strlen($string)-strlen($item)-$index;  
+	        return $index;  
+	    }  
+	        else  
+	        return -1;  
+	}
+	$currentPos = lastIndexOf($_SERVER['SCRIPT_NAME'],'/');
+	$currentCount = count($_SERVER['SCRIPT_NAME']) - 5;
+	$currentName = substr($_SERVER['SCRIPT_NAME'], $currentPos + 5, $currentCount);
+	?></dt>
 	<dd>
 		<ul>
 		<?php
+		
 		sort($filelist);
 		foreach ($filelist as $value) {
 			$count = count($value) - 5;
 			$name = substr($value, 4, $count);
-			echo '<li class="template"><a href="'.$mydir.$value.'">'.$name.'</a></li>';
+			$klass = 'template';
+			if ($name == $currentName) {
+				$klass .= ' current';
+			}
+			echo '<li class="' . $klass . '"><a href="'.$mydir.$value.'">'.$name.'</a></li>';
 		}
 		?>
 		</ul>
